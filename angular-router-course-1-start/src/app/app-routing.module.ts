30a0bceb5ab8982b5c68e2f5e001a0b2afe8a143
guiziis <guigamoda@hotmail.com>
2025-10-18 18:53:48 -0300

    not found route

--- angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-router-course-1-start/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'courses',
     loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
