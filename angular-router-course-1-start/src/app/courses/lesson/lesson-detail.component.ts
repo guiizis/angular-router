1578d1180503ed95168be14dd8939089bb77d5ae
guiziis <guigamoda@hotmail.com>
2025-10-19 15:48:33 -0300

    add lesson-detail component

--- angular-router-course-1-start/src/app/courses/lesson/lesson-detail.component.ts
+++ angular-router-course-1-start/src/app/courses/lesson/lesson-detail.component.ts
@@ -13,15 +13,16 @@ import {map} from "rxjs/operators";
 export class LessonDetailComponent implements OnInit {
 
   lesson$: Observable<LessonDetail>;
+  lesson: LessonDetail;
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
 
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
-
+      this.lesson = this.route.snapshot.data['lesson'];
   }
 
 
---
+++ angular-router-course-1-start/src/app/courses/services/lesson-detail.resolver.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { LessonSummary } from "../model/lesson-summary";
+import { CoursesService } from "./courses.service";
+import { Observable } from "rxjs";
+import { LessonDetail } from "../model/lesson-detail";
+import { first } from "rxjs/operators";
+
+@Injectable()
+export class LessonDetailResolver implements Resolve<LessonDetail> {
+  constructor(private courseService: CoursesService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<LessonDetail> {
+    return this.courseService.loadLessonDetail(route.parent.params['courseUrl'], route.params['lessonSeqNo']).pipe(
+      first()
+    );
+  }
+}
