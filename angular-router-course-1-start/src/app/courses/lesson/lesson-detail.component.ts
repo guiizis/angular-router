84eca47dc58bb45a80ce3e6f00a255930e1bdff3
guiziis <guigamoda@hotmail.com>
2025-10-19 16:06:17 -0300

    convert lesson to lesson$ to reactive route change

--- angular-router-course-1-start/src/app/courses/lesson/lesson-detail.component.ts
+++ angular-router-course-1-start/src/app/courses/lesson/lesson-detail.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {LessonDetail} from "../model/lesson-detail";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { LessonDetail } from "../model/lesson-detail";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'lesson',
@@ -13,18 +13,23 @@ import {map} from "rxjs/operators";
 export class LessonDetailComponent implements OnInit {
 
   lesson$: Observable<LessonDetail>;
-  lesson: LessonDetail;
 
-  constructor(private route: ActivatedRoute) {
-
+  constructor(private route: ActivatedRoute, private router: Router) {
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
-      this.lesson = this.route.snapshot.data['lesson'];
+    this.lesson$ = this.route.data.pipe(
+      map(data => data['lesson'])
+    );
   }
 
+  previous(lesson: LessonDetail) {
+    this.router.navigate(['lessons', lesson.seqNo - 1], { relativeTo: this.route.parent })
+  }
 
-
+  next(lesson: LessonDetail) {
+    this.router.navigate(['lessons', lesson.seqNo + 1], { relativeTo: this.route.parent })
+  }
 }
